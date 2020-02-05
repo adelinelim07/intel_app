4ee2af891ec3b23cee032fb693a49ea2637b6e69
Adeline Lim <adelinelim07@gmail.com>
2020-02-05 15:36:47 +0900

    css for login form completed

--- intel_app_frontend/src/components/registrations/Login.js
+++ intel_app_frontend/src/components/registrations/Login.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 
-
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -61,42 +60,78 @@ class Login extends Component {
       </div>
     );
   };
-  
+
   render() {
     const { username, email, password } = this.state;
     return (
       <div>
-        <h1>Log In</h1>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            placeholder="username"
-            type="text"
-            name="username"
-            value={username}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="email"
-            type="text"
-            name="email"
-            value={email}
-            onChange={this.handleChange}
-          />
-          <input
-            placeholder="password"
-            type="password"
-            name="password"
-            value={password}
-            onChange={this.handleChange}
-          />
-          <button placeholder="submit" type="submit">
-            Log In
-          </button>
-          <div>
-            or <Link to="/signup">sign up</Link>
+        <head>
+          <link rel="stylesheet" type="text/css" href="../../css/Forms.css" />
+        </head>
+        <div class="bg_img"></div>
+        <div class="form_wrapper">
+          <div class="form_container">
+            <div class="title_container">
+              <h2>Log In</h2>
+            </div>
+            <form onSubmit={this.handleSubmit}>
+              {/* <div class="row clearfix">
+                <div class="col_half">
+                  <label>Username</label>
+                  <div class="input_field">
+                    {" "}
+                    <span>
+                      <i aria-hidden="true" class="fa fa-user"></i>
+                    </span>
+                    <input
+                      placeholder="username"
+                      type="text"
+                      name="username"
+                      value={username}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+              </div> */}
+              <div class="row clearfix">
+                <div class="col_half">
+                  <label>Email</label>
+                  <div class="input_field">
+                    {" "}
+                    <span>
+                      <i aria-hidden="true" class="fa fa-envelope"></i>
+                    </span>
+                    <input
+                      placeholder="email"
+                      type="text"
+                      name="email"
+                      value={email}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+                <div class="col_half">
+                  <label>Password</label>
+                  <div class="input_field">
+                    {" "}
+                    <span>
+                      <i aria-hidden="true" class="fa fa-phone"></i>
+                    </span>
+                    <input
+                      placeholder="password"
+                      type="password"
+                      name="password"
+                      value={password}
+                      onChange={this.handleChange}
+                    />
+                  </div>
+                </div>
+              </div>
+              <input class="button" type="submit" value="Log In" />
+            </form>
           </div>
-        </form>
-        <div>{this.state.errors ? this.handleErrors() : null}</div>
+          <div>{this.state.errors ? this.handleErrors() : null}</div>
+        </div>
       </div>
     );
   }
