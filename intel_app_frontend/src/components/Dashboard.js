2084bc7f8a2d8d8334c631087b9ae33a1f188c2e
Adeline Lim <adelinelim07@gmail.com>
2020-02-10 17:00:24 +0800

    hide x-scroll

--- intel_app_frontend/src/components/Dashboard.js
+++ intel_app_frontend/src/components/Dashboard.js
@@ -1,30 +1,27 @@
 import React, { Component } from "react";
 import SideBarMenu from "./SideMenuBar.js";
-import Cookies from 'js-cookie'
+import Cookies from "js-cookie";
 
 class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: this.props.user,
+      user: this.props.user
     };
   }
 
   render() {
     return (
       <div>
-      <head>
-          <link
-            rel="stylesheet"
-            type="text/css"
-            href="/css/Dashboard.css"
-          />
+        <head>
+          <link rel="stylesheet" type="text/css" href="/css/Dashboard.css" />
         </head>
         <body>
           <div class="grid">
-          <SideBarMenu 
-          user={this.props.user} 
-          handleLogout={this.props.handleLogout}/>
+            <SideBarMenu
+              user={this.props.user}
+              handleLogout={this.props.handleLogout}
+            />
           </div>
         </body>
       </div>
