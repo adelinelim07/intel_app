49a328fb8575a5da06eb209f8cda30c55d9d8dc8
Adeline Lim <adelinelim07@gmail.com>
2020-02-13 18:02:03 +0800

    added loader for dailynews pg

--- intel_app_frontend/src/components/DailyNewsTop5.js
+++ intel_app_frontend/src/components/DailyNewsTop5.js
@@ -4,6 +4,7 @@ class DailyNewsTop5 extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isLoading: true,
       keywords: []
     };
   }
@@ -15,44 +16,68 @@ class DailyNewsTop5 extends Component {
   getKeywords() {
     fetch("http://localhost:3001/keywords")
       .then(response => response.json())
-      .then(json => this.setState({ keywords: json }))
+      .then(json => this.setState({ 
+        keywords: json,
+        isLoading: false,
+       }))
       .catch(error => console.error(error));
     console.log(this.state.keywords);
+    
   }
 
-  createChart = (count) => {
-    let chart = []
-    for (let i=0; i <count; i++) {
-      chart.push
-      (<span>
-        <i class="material-icons md-18">notifications</i>
-      </span>)
+  createChart = count => {
+    let chart = [];
+    for (let i = 0; i < count; i++) {
+      chart.push(
+        <span>
+          <i class="material-icons md-18">notifications</i>
+        </span>
+      );
     }
-    return chart
-  }
+    return chart;
+  };
 
   render() {
     return (
       <div>
         <head>
+          <link rel="stylesheet" type="text/css" href="/css/DailyNews.css" />
           <link
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
             rel="stylesheet"
-            type="text/css"
-            href="/css/DailyNews.css"
-          />
-          <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"></link>
+          ></link>
         </head>
         <body>
           <div class="top5-subheader">Today's Top Keywords</div>
           <div class="top5-content">
             {this.state.keywords.map(keyword => {
               return (
-                  <li>
-                    <div class="keyword">{keyword.keyword}</div>
-                    <div class="chart">{this.createChart(keyword.count)}</div>
-                  </li>
+                <li>
+                  <div class="keyword">{keyword.keyword}</div>
+                  <div class="chart">{this.createChart(keyword.count)}</div>
+                </li>
               );
             })}
+
+            {this.state.isLoading ? (
+              <div class="loader-wrapper">
+                {" "}
+                <div class="lds-spinner">
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                  <div></div>
+                </div>
+              </div>
+            ) : null}
           </div>
         </body>
       </div>
